0b9513b6813876d2b8f773133a5f42dca4d4ae10
IT Solutions <mstelter@itsolutionsco.com>
2022-04-27 07:12:09 -0600

    removed dart io / platform for better web compat

--- lib/src/place_picker.dart
+++ lib/src/place_picker.dart
@@ -13,7 +13,6 @@ import 'package:google_maps_place_picker/src/utils/uuid.dart';
 import 'package:google_maps_webservice/places.dart';
 import 'package:http/http.dart';
 import 'package:provider/provider.dart';
-import 'dart:io' show Platform;
 
 enum PinState { Preparing, Idle, Dragging }
 enum SearchingState { Idle, Searching }
@@ -276,8 +275,7 @@ class _PlacePickerState extends State<PlacePicker> {
         widget.automaticallyImplyAppBarLeading
             ? IconButton(
                 onPressed: () => Navigator.maybePop(context),
-                icon: Icon(
-                  Platform.isIOS ? Icons.arrow_back_ios : Icons.arrow_back,
+                icon: Icon(Icons.arrow_back,
                 ),
                 padding: EdgeInsets.zero)
             : SizedBox(width: 15),
